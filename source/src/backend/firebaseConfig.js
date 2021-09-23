ba47674bc185057bf5933037f186ca4d8cdad3d7
Arceus1ooo <56331076+Arceus1ooo@users.noreply.github.com>
2021-09-23 15:52:25 -0400

    added firebase and maps api config

---
+++ source/src/backend/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBFpcdllkznIC89VE4woqklQE1Em9g_HXw",
+  authDomain: "oasis-3e325.firebaseapp.com",
+  projectId: "oasis-3e325",
+  storageBucket: "oasis-3e325.appspot.com",
+  messagingSenderId: "211092862445",
+  appId: "1:211092862445:web:e022e187160e4f64d863e4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
