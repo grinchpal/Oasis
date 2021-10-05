2ba237b2dba2bd5ce680651e5fdd4f8f33c2d7f0
Arceus1ooo <56331076+Arceus1ooo@users.noreply.github.com>
2021-10-05 15:39:25 -0400

    init navbar

--- source/src/App.js
+++ source/src/App.js
@@ -3,11 +3,13 @@ import './index.css';
 
 //components
 import Map from './MapComponent/Map';
+import Navbar from './NavbarComponent/Navbar';
 
 function App() {
 
   return (
     <main>
+      <Navbar />
       <Map />
     </main>
   );
---
+++ source/src/NavbarComponent/Navbar.js
@@ -0,0 +1,11 @@
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <div>
+            <h1>Oasis</h1>
+        </div>
+    );
+}
+
+export default Navbar;
