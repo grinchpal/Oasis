1716ac931a3beaae5f975e69cff362b4790a4d12
Kristin Kim <kristinhajinkim@gmail.com>
2021-10-19 11:06:41 -0400

    firebase functions for auth

---
+++ source/src/AccountPageComponent/SignupComponent/firebaseConfig.js
@@ -0,0 +1,93 @@
+import firebase from 'firebase'
+
+// Initialize Firebase with api key from .env
+const app = firebase.initializeApp({
+    apiKey: process.env.REACT_APP_FIREBASE_apiKey,
+    authDomain: process.env.REACT_APP_FIREBASE_authDomain,
+    projectId: process.env.REACT_APP_FIREBASE_projectId,
+    storageBucket: process.env.REACT_APP_FIREBASE_storageBucket,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_messagingSenderId,
+    appId: process.env.REACT_APP_FIREBASE_appId
+})
+
+const app = firebase.initializeApp(firebaseConfig);
+const auth = app.auth();
+const db = app.firestore();
+
+// Google authentication function
+const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+const signInWithGoogle = async () => {
+  try {
+    const res = await auth.signInWithPopup(googleProvider);
+    const user = res.user;
+    const query = await db
+      .collection("users")
+      .where("uid", "==", user.uid)
+      .get();
+    if (query.docs.length === 0) {
+      await db.collection("users").add({
+        uid: user.uid,
+        name: user.displayName,
+        authProvider: "google",
+        email: user.email,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+// using an email and password
+const signInWithEmailAndPassword = async (email, password) => {
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+    } catch (err) {
+      console.error(err);
+      alert(err.message);
+    }
+  };
+
+// registering with an email and password
+const registerWithEmailAndPassword = async (name, email, password) => {
+try {
+    const res = await auth.createUserWithEmailAndPassword(email, password);
+    const user = res.user;
+    await db.collection("users").add({
+    uid: user.uid,
+    name,
+    authProvider: "local",
+    email,
+    });
+} catch (err) {
+    console.error(err);
+    alert(err.message);
+}
+};
+
+// send password reset link
+const sendPasswordResetEmail = async (email) => {
+  try {
+    await auth.sendPasswordResetEmail(email);
+    alert("Password reset link sent!");
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+// logout
+const logout = () => {
+    auth.signOut();
+  };
+
+export {
+auth,
+db,
+signInWithGoogle,
+signInWithEmailAndPassword,
+registerWithEmailAndPassword,
+sendPasswordResetEmail,
+logout,
+};
