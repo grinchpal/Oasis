cdabd2dc1fe72e46aba715dd822c9f3d051edac3
Kristin Kim <kristinhajinkim@gmail.com>
2021-12-02 15:49:23 -0500

    add facilities suggested by Bill, and website link to all cards

--- source/src/ResourcesPageComponent/ResourceCard.js
+++ source/src/ResourcesPageComponent/ResourceCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function ResourceCard({ description, name, image, facility }) {
+export default function ResourceCard({ description, name, image, facility, website }) {
   return(
     <div className='card'>
         <img src={image}/>
@@ -20,5 +20,6 @@ ResourceCard.propTypes = {
   description: PropTypes.string.isRequired,
   facility: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
+  website: PropTypes.string.isRequired
 //   link: PropTypes.string.isRequired
 }
--- source/src/ResourcesPageComponent/data.js
+++ source/src/ResourcesPageComponent/data.js
@@ -1,25 +1,53 @@
 import oasislogo from '../assets/oasisLogo.PNG'
 import hotlinelogo from '../assets/hotline.svg'
 import hobokenlogo from '../assets/hobokenlogo.png'
+import womenrisinglogo from '../assets/womenrisinglogo.png'
+import njreentrylogo from '../assets/njreentrylogo.png'
+import hopehouselogo from '../assets/hopehouselogo.png'
 
 export default [
     {
-      name: 'Domestic Violence Support',
-      description: 'Get Help, Free. Confidential. 24/7. Get Help Free. Confidential. 24/7. Get Help',
-      image: oasislogo,
-      facility: 'Nation Women Support',
+      name: 'Women Rising - Jersey City',
+      description: 'Get Help! You are not alone (201)-333-5700 WomenRising DV Support Available Childcare Provided',
+      image: womenrisinglogo,
+      facility: 'Women Rising',
+      website: 'https://www.womenrising.org/get-help'
     //   link: 
     },
     {
-        name: 'Free Chat Support',
-        description: ' Get Help, Free. Confidential. 24/7. Get Help Free. Confidential. 24/7. Get Help',
-        image: hobokenlogo,
-        facility: 'Nation Support'
+        name: 'NJ Re-Entry Coporation',
+        description: 'With a social mission to remove all barriers to employment for citizens returning from incarceration (Food Pantry, Supportive Housing, Job Search, Training, Mental Health Support)',
+        image: njreentrylogo,
+        facility: 'NJ Reentry Corporation',
+        website: 'https://www.njreentry.org/locations'
     },
     {
-        name: 'Domestic Violence Support',
-        description: 'Free. Confidential. 24/7. Get Help Free. Confidential. 24/7. Get Help, Free. Confidential. 24/7. Get Help Free. Confidential. 24/7. Get Help',
-        image: hotlinelogo,
-        facility: 'Stevens'
-    }
+        name: 'Emergency Shelter for Homeless Families',
+        description: '(201) 420-1070 Helps families become self-sufficient by giving them an opportunity for decision making',
+        image: hopehouselogo,
+        facility: 'Hope House, Jersey City',
+        website: 'https://www.ccannj.com/hope_house.php'
+    }, 
+    {
+      name: 'Hoboken Shelter',
+      description: 'Get Help, Free. Confidential. 24/7. Get Help Free. Confidential. 24/7. Get Help',
+      image: hobokenlogo,
+      facility: 'Hoboken Shelter',
+      website: 'https://www.ccannj.com/hope_house.php'
+    },
+    {
+      name: 'Domestic Violence Support',
+      description: 'Get Help, Free. Confidential. 24/7. Get Help Free. Confidential. 24/7. Get Help',
+      image: hotlinelogo,
+      facility: 'Nation Women Support',
+      website: 'https://www.ccannj.com/hope_house.php' 
+    },
+        // {
+    //   name: 'Domestic Violence Support',
+    //   description: 'Get Help, Free. Confidential. 24/7. Get Help Free. Confidential. 24/7. Get Help',
+    //   image: oasislogo,
+    //   facility: 'Nation Women Support',
+    //   website: 'https://www.thehotline.org/'
+    // //   link: 
+    // },
   ]
